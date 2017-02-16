c72b58df4df68872904b0e73ad7df681d1a3b51a
Dan Norman <normande@pkcsecurity.com>
2017-02-16 15:50:16 -0800

    inc patch number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.pkcsecurity/ddist "0.1.0"
+(defproject com.pkcsecurity/ddist "0.1.1"
   :description "Determine how similar two documents are to one another"
   :url "https://github.com/pkcsecurity/ddist"
   :license {:name "Eclipse Public License"
